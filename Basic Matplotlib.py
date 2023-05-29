d5e9900d00bb1fee8650ee808777e3c5c3d11334
Ahmad <115799828+CodingwithAhmad1@users.noreply.github.com>
2023-05-29 12:10:21 +0400

    Add files via upload

---
+++ Basic Matplotlib.py
@@ -0,0 +1,17 @@
+# import libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Creating vectors X and Y
+# first value is far left x value, second is far right x value, third is the number of graph points
+x = np.linspace(-4, 4, 100)
+y = x ** 2  # graph equation
+
+figure = plt.figure(figsize=(10, 5))  # length by height
+
+# Create the plot
+plt.plot(x, y)
+
+# Show the plot
+plt.show()
